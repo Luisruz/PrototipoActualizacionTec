6142a40e1ad81bfadfe4b71024ca538ec545058d
ProyectoCvLAC <luis.ruz@cecar.edu.co>
2019-06-09 12:55:20 -0500

    Contactanos

--- app/src/main/java/com/example/prototipo_actualizacion_tec/MainActivity.java
+++ app/src/main/java/com/example/prototipo_actualizacion_tec/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.prototipo_actualizacion_tec;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.security.Principal;
 
 public class MainActivity extends AppCompatActivity {
+private Button btnContacto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnContacto= findViewById(R.id.buttonContacto);
+        btnContacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cambio = new  Intent(MainActivity.this, Contactanos.class);
+                startActivity(cambio);
+            }
+        });
     }
 }
